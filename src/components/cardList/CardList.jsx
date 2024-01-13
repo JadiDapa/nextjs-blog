3cf7c8ff9d5de4354e61b135173d86b62c768885
JadiDapa <daffaalthaf25@gmail.com>
2024-01-13 11:04:41 +0700

    HomePage

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,9 +1,20 @@
+import Image from "next/image";
 import styles from "./cardList.module.css";
 import Pagination from "@/components/pagination/Pagination";
+import Card from "@/components/card/Card";
 
 const CardList = () => {
   return (
     <div className={styles.container}>
+      <h1 className={styles.title}>Recent Posts</h1>
+      <div className={styles.posts}>
+        <div className={styles.post}>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
       <Pagination />
     </div>
   );
